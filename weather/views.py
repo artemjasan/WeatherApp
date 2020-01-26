43b40668b05387d46f6aa6a00117bde2853f6670
Artem Jasan <artjasan@gmail.com>
2020-01-26 16:32:59 +0100

    Maked migrate and added models.py

--- weather/views.py
+++ weather/views.py
@@ -4,13 +4,20 @@ from django.shortcuts import render
 
 def index(request):
     appid ='b1b72a99f86a510fac7e0177a66cf9ba'
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=' + appid
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
     city = 'Prague'
-    res  = requests.get(url.format(city))
-    print(res.text)
+    res  = requests.get(url.format(city)).json()
+    
+    city_info = {
+        'city':city,
+        'temp': res["main"] ["temp"] ,
+        'icon': res["weather"] [0] ["icon"],
+        'pres': res["main"]["pressure"]
+    }
 
+    context = {'info': city_info}
 
-    return render(request, 'weather/index.html')
+    return render(request, 'weather/index.html', context)
 
 # Create your views here.
